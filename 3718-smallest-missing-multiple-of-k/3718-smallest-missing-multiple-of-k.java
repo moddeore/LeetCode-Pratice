407f8aa7fd571ef5b8be4234ef8c3dd9be73f594
moddeore <moddeore2006@gmail.com>
2026-08-27 23:54:42 +0530

    Time: 0 ms (100%), Space: 45.1 MB (71.47%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        boolean[] present = new boolean[101];
+
+        for (int num : nums) {
+            present[num] = true;
+        }
+
+        int multiple = k;
+
+        while (multiple <= 100) {
+            if (!present[multiple]) {
+                return multiple;
+            }
+            multiple += k;
+        }
+
+        // If all multiples up to 100 are present
+        return multiple;
+    }
+}
